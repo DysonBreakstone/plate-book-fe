5299a4bc3f5d7c63f370a5b8c4ef4091bbede447
Andrew Bingham <andrewgbingham@gmail.com>
2023-06-03 16:50:59 -0600

    feat: update user poros

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -1,5 +1,5 @@
 class User
-  attr_reader :id, :username, :email, :uid, :token, :created_at, :updated_at
+  attr_reader :id, :username, :email, :uid, :token, :created_at, :updated_at, :comments, :plates, :posts
   
   def initialize(data)
     @id = data[:id]
@@ -9,5 +9,8 @@ class User
     @token = data[:attributes][:token]
     @created_at = data[:attributes][:created_at]
     @updated_at = data[:attributes][:updated_at]
+    @comments = data[:attributes][:comments]
+    @plates = data[:attributes][:plates]
+    @posts = data[:attributes][:posts]
   end
 end

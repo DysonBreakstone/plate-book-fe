f218482397524d230fcc1151d001b64d34bd6c6a
Young Koh <youngheon6@gmail.com>
2023-06-06 16:55:59 -0600

    feat: fix tests

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -1,10 +1,6 @@
 class BackendService
   def conn
-<<<<<<< HEAD
-    @_conn ||= Faraday.new(url: "http://#{ENV['BACK_END_DOMAIN']}:5001")
-=======
     @_conn ||= Faraday.new(url: "http://#{ENV['BACK_END_DOMAIN']}:5001/")
->>>>>>> origin
   end
 
   def register_user(google_id, email, token)
